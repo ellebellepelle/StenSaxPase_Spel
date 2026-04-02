728aa4d9bc31a64df06ba0ea5b5e90fc3d995559
Elise Elf <elise.elf@hotmail.com>
2026-04-02 14:54:20 +0200

    slumpgenerator-funktion i spel.js

--- spel.js
+++ spel.js
@@ -12,3 +12,23 @@ function namnFunktion() {
     // Stoppar in användarens namn i class=div1´s h2
     namnH2.innerText = `Tjena-nixen & Välkommen ${spelarensNamn}, Lycka till idag ❤️`
 }
+
+
+
+
+// Skapar en slumpgenerator funktion till datorn.
+function datornsVal() {
+    // en array med de val som datorn kan välja på
+    const val =  ["🪨", "✂️", "💰"];
+    /* genererar ett slumpmässigt heltal mellan 0 och 2
+    - Math.random() ger ett tal mellan 0 och 1
+    - multipliceras med 3 -> blir mellan o och 3 (ej inklusive 3)
+    - Math.floor() avrundar ner till 0, 1 eller 2 */
+    const slumpTal = Math.floor(Math.random() * 3);
+    // Returnerar ett av värderna i arrayen baserat på slumpTal (index)
+    return val[slumpTal];
+} 
+
+
+
+
